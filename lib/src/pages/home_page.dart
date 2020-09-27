a3a1797968a5a161613209496aa321baf1885fa6
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-09-26 20:09:53 -0400

    Creando Bottom Navigation Bar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +15,21 @@ class _HomePageState extends State<HomePage> {
           child: Text('Home Page'),
         ),
       ),
+      bottomNavigationBar: _crearBottomNavigationBar(),
     );
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(items: _items());
+  }
+
+  List<BottomNavigationBarItem> _items() {
+    return <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Inicio')),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.favorite), title: Text('Favoritos')),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle), title: Text('Cuenta')),
+    ];
   }
 }
