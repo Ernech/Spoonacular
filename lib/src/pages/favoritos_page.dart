6b4002c57dea8702be77d1592ff1337e196f7055
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-09-26 20:19:06 -0400

    Agregando funcionalidad al bottom navigation drawer

---
+++ lib/src/pages/favoritos_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoritosPage extends StatelessWidget {
+  const FavoritosPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Favoritos Page'),
+      ),
+    );
+    ;
+  }
+}
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spoonacular/src/pages/cuenta_page.dart';
+import 'package:spoonacular/src/pages/favoritos_page.dart';
+import 'package:spoonacular/src/pages/inicio_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,17 +13,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home Page'),
-        ),
-      ),
+      body: _cargarPagina(_currentIndex),
       bottomNavigationBar: _crearBottomNavigationBar(),
     );
   }
 
   Widget _crearBottomNavigationBar() {
-    return BottomNavigationBar(items: _items());
+    return BottomNavigationBar(
+      items: _items(),
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+    );
   }
 
   List<BottomNavigationBarItem> _items() {
@@ -31,5 +38,21 @@ class _HomePageState extends State<HomePage> {
       BottomNavigationBarItem(
           icon: Icon(Icons.account_circle), title: Text('Cuenta')),
     ];
+  }
+
+  Widget _cargarPagina(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return InicioPage();
+        break;
+      case 1:
+        return FavoritosPage();
+        break;
+      case 2:
+        return CuentaPage();
+        break;
+      default:
+        return InicioPage();
+    }
   }
 }
