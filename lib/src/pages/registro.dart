d4c7ed5d2c3187cef4eebe38eadb211b302b9f95
ArielColqueH <arielcolque444@gmail.com>
2020-11-01 12:14:54 -0400

    registrate page improved

--- lib/src/pages/registro.dart
+++ lib/src/pages/registro.dart
@@ -3,6 +3,8 @@ import 'package:spoonacular/src/bloc/login_bloc.dart';
 import 'package:spoonacular/src/bloc/provider.dart';
 
 import 'package:spoonacular/src/providers/usuario_provider.dart';
+import 'package:spoonacular/src/widgets/custome_input.dart';
+import 'package:spoonacular/src/widgets/custome_input_account.dart';
 import 'package:spoonacular/src/widgets/line_circule_detail.dart';
 import 'package:spoonacular/utils/utils.dart';
 import '../../constants.dart';
@@ -27,61 +29,64 @@ class _RegistroState extends State<Registro> {
               new Container(
                 child: new Image.asset(
                   'images/logo.png',
-                  height: 160.0,
+                  height: 140.0,
                   fit: BoxFit.cover,
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
               Text(
                 "Registrate en Nutrana",
                 style: TextStyle(
-                  fontSize: 35,
+                  fontSize: 30,
                   fontFamily: "Pacifico",
                   color: Color(0xFF5B8C2A),
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
-              //CustomeInput("Usuario", Icons.person, "Nombre de Usuario"),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Nombre de Usuario",
-                ),
-                validator: (String val) {
-                  if (val.isEmpty) {
-                    return "Ingrese algun texto";
-                  }
-                  return null;
-                },
-              ),
+              // //CustomeInput("Usuario", Icons.person, "Nombre de Usuario"),
+              // TextFormField(
+              //   decoration: const InputDecoration(
+              //     labelText: "Nombre de Usuario",
+              //   ),
+              //   validator: (String val) {
+              //     if (val.isEmpty) {
+              //       return "Ingrese algun texto";
+              //     }
+              //     return null;
+              //   },
+              // ),
+              CustomeInputAccount("Nombre", Icons.person, "Nombre"),
               SizedBox(
-                height: 20,
+                height: 10,
+              ),
+              CustomeInputAccount("Apellido Paterno", Icons.person, "Apellido Paterno"),
+              SizedBox(
+                height: 10,
+              ),
+              CustomeInputAccount("Apellido Materno", Icons.person, "Apellido Materno"),
+              SizedBox(
+                height: 10,
               ),
               //CustomeInput("Correo", Icons.person, "Correo Electronico"),
               _crearEmail(loginBloc),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
               // CustomeInput("Contraseña", Icons.lock, "Contraseña"),
               _crearPassword(loginBloc),
-
-              SizedBox(
-                height: 20,
-              ),
               SizedBox(
                 height: 20,
               ),
               _crearBoton(loginBloc),
               SizedBox(
-                height: 40,
+                height: 10,
               ),
               LineCirculeDetail(),
-              SizedBox(
-                height: 20,
-              ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -116,29 +121,129 @@ class _RegistroState extends State<Registro> {
   }
 
   Widget _crearEmail(LoginBloc loginbloc) {
-    return StreamBuilder(
-        stream: loginbloc.emailStream,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return TextField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: loginbloc.changeEmail,
-            decoration: InputDecoration(
-                labelText: "Correo Electronico", errorText: snapshot.error),
-          );
-        });
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 34),
+              child: Text(
+                "Correo ELectronico",
+                style: TextStyle(
+                    color: primaryGreen,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 32),
+          padding: EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(
+              color: Colors.white,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 1.0), //(x,y)
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: StreamBuilder(
+              stream: loginbloc.emailStream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: loginbloc.changeEmail,
+                  decoration: InputDecoration(
+                    errorText: snapshot.error,
+                    border: InputBorder.none,
+                    icon: Icon(
+                      Icons.person,
+                      color: Colors.grey,
+                    ),
+                    hintText: "Email",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
   }
 
   Widget _crearPassword(LoginBloc loginbloc) {
-    return StreamBuilder(
-        stream: loginbloc.passwordStream,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return TextFormField(
-            onChanged: loginbloc.changePassword,
-            obscureText: true,
-            decoration: InputDecoration(
-                labelText: "Contraseña", errorText: snapshot.error),
-          );
-        });
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 34),
+              child: Text(
+                "Contraseña",
+                style: TextStyle(
+                    color: primaryGreen,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 32),
+          padding: EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(
+              color: Colors.white,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0.0, 1.0), //(x,y)
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: StreamBuilder(
+              stream: loginbloc.passwordStream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return TextFormField(
+                  onChanged: loginbloc.changePassword,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    errorText: snapshot.error,
+                    icon: Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                    hintText: "Contrasena",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
   }
 
   Widget _crearBoton(LoginBloc loginbloc) {
